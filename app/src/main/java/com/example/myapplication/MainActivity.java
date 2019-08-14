9cc93e585096a6bface1f01e8ee8a57de891b9a0
undersense <undersense@naver.com>
2019-08-14 10:57:34 +0900

    test1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
